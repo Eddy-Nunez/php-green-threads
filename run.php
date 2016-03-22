2742b4454d24e32be1a1a65c28823523f1825e0e
Eddy Nunez <eddynunez@yahoo.com>
2016-03-21 21:10:29 -0400

    added another type of callable

--- run.php
+++ run.php
@@ -7,6 +7,13 @@ $pool = new GreenThreadPool;
 $pool->add( 'printWork', array() );
 $pool->add( 'fileWork', [ 'filename' => 'workers.php' ] );
 
+// $pool->add( function($acc) {
+//     $sec = date('s');
+//     print "secs = $sec" . PHP_EOL;
+//     sleep(1);
+//     return !($sec >= 55 && $sec <= 59);
+// }, [] );
+
 while ( !$pool->isDone() )
     $pool->run();
 
